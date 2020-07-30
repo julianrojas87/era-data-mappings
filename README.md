af8064e1bea70fb7ec78f164e562451feee1afc9
Julian Rojas <julian.rojas87@gmail.com>
2020-07-30 03:23:43 +0300

    Added sources

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # era-data-mappings
-RML mappings for ERA's RINF and ERATV databases
+
+RML mappings for the ERA project
+
+## Requirements
+
+- Node.js
+- Java JRE
+
+## Execute mappings
+
+The mappings can be executed using the `map-turtle.sh`  or `map-ntriples.sh` scripts. These script will run the mapping process for all the YARRRML files present in the `mappings/` folder. The resulting Linked Data files will be stored in the `knowledge-graph/` folder.
+
+```bash
+./map-turtle.sh
+```
---
+++ map-turtle.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+trap exit 1 SIGINT
+
+mappings_directory="mappings"
+output_directory="knowledge-graph"
+rml_directory="rml"
+
+mkdir -p ${output_directory}
+mkdir -p ${rml_directory}
+
+downloadYARRRML() {
+    local yarrrml_dir="yarrrml-parser"
+    if [ ! -d $yarrrml_dir ]; then
+        git clone --branch v1.1.1 https://github.com/RMLio/yarrrml-parser.git $yarrrml_dir
+        cd $yarrrml_dir
+        npm i
+        cd -
+    fi
+}
+
+parseYARRRML() {
+    local yarrrml_file_path=$1
+    local yarrrml_file_name=$2
+    local output_file_extension=$3
+
+    ./yarrrml-parser/bin/parser.js -i $yarrrml_file_path -o "${rml_directory}/${yarrrml_file_name}_rml.$output_file_extension"
+}
+
+mapRML() {
+    local file_path=$1
+    local output_file=$2
+
+    if [ $(stat -c %s $output_file 2>/dev/null || echo -e 0) -gt 0 ]; then
+        echo "$file_path has already been mapped"
+    else
+        echo "Mapping '$file_path' and storing output in '$output_file'"
+        time java -Xmx4096m -cp rmlmapper-4.7.0-r210.jar:mssql-jdbc-8.2.0.jre11.jar be.ugent.rml.cli.Main -s turtle -m $file_path > $output_file
+    fi
+}
+
+downloadYARRRML
+
+# Parse YARRRML files to RML
+for f in ${mappings_directory}/*.yml; do
+    file=${f##*/}
+    echo "Generating RML mappings from '${f##*/}'"
+    parseYARRRML "$f" "${file%.*}" "ttl"
+done
+
+# Execute RML mappings
+for f in ${rml_directory}/*.ttl; do
+    file=${f##*/}
+    mapRML "${f}" "${output_directory}/${file%_rml*}.ttl"
+done
